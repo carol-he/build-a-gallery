f03a8523f3e4e6921ed1d1f09c1dc853595b0469
Carol He <ch2727@nyu.edu>
2018-05-01 21:14:55 -0400

    custom buttons

--- build-a-gallery/upload/index.php
+++ build-a-gallery/upload/index.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <title>Upload Images</title>
     <link rel="stylesheet" type="text/css" href="../../styles/upload.css">
+<!--
+    <link rel="stylesheet" type="text/css" href="../../styles/skeleton.css">
+    <link rel="stylesheet" type="text/css" href="../../styles/normalize.css">
+-->
     <link href="https://fonts.googleapis.com/css?family=Julius+Sans+One" rel="stylesheet">
   </head>
   <body>
@@ -26,12 +30,18 @@
     <div id="upload">
       <form method="post" action="upload.php" enctype="multipart/form-data">
         please choose images you would like to use in the gallery simulator.<br><br><br>
-        <input type="file" name="file" id="file">
+        <div class="file-upload">
+          <div class="file-select">
+            <div class="file-select-button" id="fileName">Choose File</div>
+            <div class="file-select-name" id="noFile">No file chosen...</div> 
+            <input type="file" name="chooseFile" id="chooseFile" class="button">
+          </div>
+        </div>
         <input type="submit" value="Upload Image">
       </form>
       <div id="buttonBox">
-        <a href="../index.php" class="right"><div id="rightButton">Proceed to image assignment</div></a>
         <a href="../../index.php" class="left"><div id="leftButton">Back</div></a>
+        <a href="../index.php" class="right"><div id="rightButton">Proceed to image assignment</div></a>
       </div>
     </div>
 
